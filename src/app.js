db556de2bd6199936defd7cbaef22e2f3f2b0c8a
Pablo Ortega <p.ortegariera@gmail.com>
2023-11-08 19:00:20 -0300

    feat: CORS config added

--- src/app.js
+++ src/app.js
@@ -1,9 +1,24 @@
 import express from "express";
 import morgan from "morgan";
+import cors from "cors";
 import passport from "./passport/passportConfig.js";
 import indexRouter from "./routes/index.routes.js";
 
 const app = express();
+
+const allowedOrigins = [process.env.FRONTEND_URL];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(morgan("dev"));
 
