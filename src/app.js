d4c3ee49a7eea5547557f323d6b0d5f74147e9ca
Pablo Ortega <p.ortegariera@gmail.com>
2023-11-08 19:40:41 -0300

    refactor: Change cors options

--- src/app.js
+++ src/app.js
@@ -6,15 +6,9 @@ import indexRouter from "./routes/index.routes.js";
 
 const app = express();
 
-const allowedOrigins = [process.env.FRONTEND_URL];
 const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+  origin: process.env.FRONTEND_URL, 
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true,
 };
 
