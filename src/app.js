2f9df48b9d2a8242a44b85c6d3dc19c1acf51b0f
Pablo Ortega <p.ortegariera@gmail.com>
2023-11-09 08:46:08 -0300

    refactor: cors

--- src/app.js
+++ src/app.js
@@ -6,19 +6,17 @@ import indexRouter from "./routes/index.routes.js";
 
 const app = express();
 
-app.use(cors());
+const corsOptions = {
+  origin: process.env.FRONTEND_URL,
+  credentials: true,
+  methods: ["GET", "POST", "OPTIONS", "PUT", "DELETE"],
+  allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "Authorization"],
+};
+console.log(corsOptions)
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(morgan("dev"));
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", process.env.FRONTEND_URL); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Access-Control-Request-Headers,Authorization"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
-});
+
 app.use(passport.initialize());
 
 app.use("/", indexRouter);
