a8a3b09f1c6d6a6358c0319ca59bf814e28107c0
Pablo Ortega <p.ortegariera@gmail.com>
2023-11-09 08:26:03 -0300

    refactor: change cors strategy

--- src/app.js
+++ src/app.js
@@ -6,16 +6,19 @@ import indexRouter from "./routes/index.routes.js";
 
 const app = express();
 
-const corsOptions = {
-  origin: process.env.FRONTEND_URL, 
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  credentials: true,
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", process.env.FRONTEND_URL); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Access-Control-Request-Headers,Authorization"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
 app.use(passport.initialize());
 
 app.use("/", indexRouter);
